9faf3aa0f8880f23d8ce432058dcd0ade5ed03cc
Elliot Landolt <112653805+elliot-landolt@users.noreply.github.com>
2024-11-11 18:12:02 -0500

    Unemployment report QC (#1)
    * Refactored unemployment report
    
    * unemployment test

--- app/unemployment_report.py
+++ app/unemployment_report.py
@@ -1,39 +1,47 @@
-import requests
 import json
 from pprint import pprint
-from plotly.express import line
 from statistics import mean
+
+import requests
+from plotly.express import line
 from app.alpha import API_KEY
 
-request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
+def fetch_unemployment_json():
 
-response = requests.get(request_url)
+    request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
 
-parsed_response = json.loads(response.text)
-print(type(parsed_response))
-pprint(parsed_response)
+    response = requests.get(request_url)
 
-data = parsed_response["data"]
+    parsed_response = json.loads(response.text)
+    print(type(parsed_response))
+    pprint(parsed_response)
 
-# Challenge A
-print("-------------------------")
-print("LATEST UNEMPLOYMENT RATE:")
-#print(data[0])
-print(f"{data[0]['value']}%", "as of", data[0]["date"])
+    data = parsed_response["data"]
+    for item in data:
+        item["value"] = float(item["value"])
+    return data
 
-# Challenge B
-this_year = [d for d in data if "2022-" in d["date"]]
+if __name__ == "__main__":
+    data = fetch_unemployment_json()
+    # Challenge A
+    print("-------------------------")
+    print("LATEST UNEMPLOYMENT RATE:")
+    #print(data[0])
+    print(f"{data[0]['value']}%", "as of", data[0]["date"])
 
-rates_this_year = [float(d["value"]) for d in this_year]
-#print(rates_this_year)
+    # Challenge B
+    this_year = [d for d in data if "2022-" in d["date"]]
 
-print("-------------------------")
-print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
-print("NO MONTHS:", len(this_year))
+    rates_this_year = [float(d["value"]) for d in this_year]
+    #print(rates_this_year)
 
-#Challenge C
-dates = [d["date"] for d in data]
-rates = [float(d["value"]) for d in data]
+    print("-------------------------")
+    print("AVG UNEMPLOYMENT THIS YEAR:", f"{mean(rates_this_year)}%")
+    print("NO MONTHS:", len(this_year))
 
-fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
-fig.show()
+    #Challenge C
+    dates = [d["date"] for d in data]
+    rates = [float(d["value"]) for d in data]
+
+    fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", labels= {"x": "Month", "y": "Unemployment Rate"})
+    fig.show()
